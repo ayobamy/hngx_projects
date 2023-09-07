b59c0877b00212456f9859c1d2dde08b8d3e263f
ayobamy <ayobamyahmed@gmail.com>
2023-09-07 16:56:34 +0100

    [Update]: home route

--- stage_one/routes/index.js
+++ stage_one/routes/index.js
@@ -3,7 +3,6 @@ import AppController from "../controllers/AppController.js";
 
 const router = express.Router();
 
-router.get('/home', AppController.getHome);
 router.get('/', AppController.slackAndTrack);
 
 export default router;
