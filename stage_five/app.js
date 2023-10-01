95eb8d47483d25488fb02ed9cc463e861d75330c
ayobamy <ayobamyahmed@gmail.com>
2023-10-01 18:37:39 +0100

    [Feat]: Video endpoint

--- stage_five/app.js
+++ stage_five/app.js
@@ -1,6 +1,5 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import rateLimit from 'express-rate-limit';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
@@ -15,8 +14,6 @@ const app = express();
 app.use(cors());
 app.options('*', cors());
 
-app.disable('x-powered-by');
-
 const NODE_ENV = process.env.NODE_ENV;
 
 // sets appropriate headers in the HTTP responses
@@ -25,14 +22,6 @@ app.use(helmet());
 if (NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
-// limit the number of requests per minute
-const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000,
-  max: 25,
-  message: 'Too many request from this IP address. Please try again later!',
-});
-app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
