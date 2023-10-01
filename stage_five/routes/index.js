537a584c81d86ca97ddeee3d4130f8757c3c4a15
ayobamy <ayobamyahmed@gmail.com>
2023-10-01 11:42:04 +0100

    [Feat]: Video endpoint

--- stage_five/routes/index.js
+++ stage_five/routes/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import multer from 'multer';
-import path from 'path';
 import AppController from "../controllers/AppController.js";
 
 const router = express.Router();
